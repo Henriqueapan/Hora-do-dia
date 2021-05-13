fa59be64153ca70b0e96dce66554192e194a567f
Henriqueapan <84114633+Henriqueapan@users.noreply.github.com>
2021-05-13 03:43:11 -0300

    Input de horário e mudanças no texto principal
    Input de horário e ícone adicionados e mudanças no script relacionadas ao texto principal.

--- js/script.js
+++ js/script.js
@@ -1,25 +1,64 @@
+var data = new Date()
+var min = data.getMinutes()
+var hora = data.getHours()
+
 function horario() {
-    var msg = document.getElementById("d1")
-    var imag = document.getElementById("imagem")
-    var data = new Date()
-    var hora = data.getHours()
-    //var hora = 10
+    var msg = document.querySelector("#d1")
+    var imag = document.querySelector("#imagem")
     var corpo = document.querySelector("body")
 
-    if (hora > 18 || hora < 6){
-        msg.innerHTML = `Boa noite! Agora são ${hora} horas.`    
+    if (hora >= 18 || hora < 6) {
+        msg.innerHTML = "Boa noite!"
+        if(hora != 0 && hora != 1) {
+            msg.innerHTML += ` Agora são ${hora} horas` 
+        }
+        else if (hora == 1) {
+            msg.innerHTML += ` Agora é 1 hora`            
+        }
+        else {
+            msg.innerHTML += ` Agora é meia noite`
+        }
         imag.src = 'img/fotonoite.png'
         corpo.style.background = "#0c2461"
     }
-    else if (hora > 5 && hora < 12){
-        msg.innerHTML = `Bom dia! Agora são ${hora} horas.`
+    else if (hora > 5 && hora < 12) {
+        msg.innerHTML = `Bom dia! Agora são ${hora} horas`
         imag.src = 'img/fotodia.png'
         corpo.style.background = "#38ada9"
     }
-    else {
-        msg.innerHTML = `Boa tarde! Agora são ${hora} horas.`
+    else if (hora >= 12 && hora < 18) {
+        msg.innerHTML = "Boa tarde!"
+        if(hora == 12 ){
+            msg.innerHTML += ` Agora é meio dia`
+        }
+        else {
+            msg.innerHTML += ` Agora são ${hora} horas`
+        }
         imag.src = 'img/fototarde.png'
         corpo.style.background = "#f8c291"
     }
+    if (min != 0 && min != 1 && min != 30) {
+        msg.innerHTML += ` e ${min} minutos.`
+    }
+    else if (min == 0) {
+        msg.innerHTML += ` em ponto.`
+    }
+    else if (min == 30) {
+        msg.innerHTML += ` e meia.`
+    }
+    else {
+        msg.innerHTML += ` e 1 minuto.`
+    }
 
 }
+
+function input() {
+    var hora_input = document.querySelector("input#user-ctime").value.split(':')[0]
+    var min_input = document.querySelector("input#user-ctime").value.split(':')[1]
+    hora = Number(hora_input);
+    min = Number(min_input)
+    horario()
+}
+
+
+
